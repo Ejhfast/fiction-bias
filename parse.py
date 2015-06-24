64033b5a9ccba203aaa258f2453c4589c1c237b8
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-06-24 04:58:59 +0000

    counts verb freq for genders using dependency trees

--- parse.py
+++ parse.py
@@ -3,16 +3,22 @@ from collections import defaultdict
 import spacy.en
 
 freq_h = defaultdict(int)
-
-freq_h["rando"] += 1
+freq_s = defaultdict(int)
+#freq_h["rando"] += 1
 
 # for k,v in freq_h.iteritems():
   # iterate over dictionary keys, values
 
 nlp = spacy.en.English()
 
-x = [1,2,3]
-y = [z*z for z in x if z > 1]
+#x = [1,2,3]
+#y = [z*z for z in x if z > 1]
+
+#def updateVerbFreq(tk, freq):
+#  for x in tk.children:
+#    if(x.pos_ == "VERB"):
+#      print (tk.lower_, x.lower_)
+#      freq[x.lower_] +=1
 
 for line in fileinput.input():
   id_, chapter = [x.rstrip() for x in line.split("\t")]
@@ -21,7 +27,19 @@ for line in fileinput.input():
     try:
       tokens = nlp(unicode(sen),tag=True,parse=True)
       for tk in tokens:
-        #if(tk.pos_ == "VERB"):
-        print(tk.pos_, tk.lemma_, tk.lemma, tk.orth_, tk.orth, tk.dep_, [x.orth_ for x in tk.children])
+        if(tk.pos_ == "VERB"):
+           for x in tk.children:
+              if(x.pos_ == "PRON"):
+                if (x.lower_ == "she"):
+                   # print (tk.lower_, x.lower_)
+                   freq_s[tk.lower_] +=1
+                   # print freq_s[tk.lower_]
+                elif (x.lower_ == "he"):
+                   # print (tk.lower_, x.lower_)
+                   freq_h[tk.lower_] +=1
+                   # print freq_h[tk.lower_]
+             
+            
+          #print(tk.pos_, tk.lemma_, tk.lemma, tk.lower_, tk.lower, tk.dep_, [x.lower_ for x in tk.children])
     except UnicodeDecodeError:
       pass
