ae32b89ed7d6a8c7110973961636108d65ebff7a
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-06-26 21:19:09 +0000

    added freq for his and her and nouns

--- parse.py
+++ parse.py
@@ -7,10 +7,14 @@ freq_h = defaultdict(int)
 freq_s = defaultdict(int)
 freq_her = defaultdict(int)
 freq_him = defaultdict(int)
+freq_her_pos = defaultdict(int)
+freq_his = defaultdict(int)
 tot_s = 0.0
 tot_h = 0.0
 tot_her = 0.0
 tot_him = 0.0
+tot_her_pos = 0.0
+tot_his = 0.0
 
 # for k,v in freq_h.iteritems():
   # iterate over dictionary keys, values
@@ -20,11 +24,10 @@ nlp = spacy.en.English()
 #x = [1,2,3]
 #y = [z*z for z in x if z > 1]
 
-#def updateVerbFreq(tk, freq):
+#def findObjFreq(tk, freq):
 #  for x in tk.children:
-#    if(x.pos_ == "VERB"):
-#      print (tk.lower_, x.lower_)
-#      freq[x.lower_] +=1
+#     if(x.pos_ == "NOUN"):
+#        freq[x.lemma_ += 1]
 
 for line in fileinput.input():
   id_, chapter = [x.rstrip() for x in line.split("\t")]
@@ -33,6 +36,7 @@ for line in fileinput.input():
     try:
       tokens = nlp(unicode(sen),tag=True,parse=True)
       for tk in tokens:
+      
         if(tk.pos_ == "VERB"):
            for x in tk.children:
               if(x.pos_ == "PRON"):
@@ -40,7 +44,6 @@ for line in fileinput.input():
                    # print (tk.lower_, x.lower_)
                    tot_s += 1
                    freq_s[tk.lemma_] +=1
-                   # print freq_s[tk.lower_]
                 elif (x.lower_ == "he"):
                    # print (tk.lower_, x.lower_)
                    tot_h += 1
@@ -52,11 +55,17 @@ for line in fileinput.input():
                 elif (x.lower_ == "him"):
                    tot_him += 1
                    freq_him[tk.lemma_] +=1
-             
-#         if(tk.pos_ == "PRON"):
-#            parent = tk.parent
-#            print(parent)
-            # print(tk.pos_, tk.lemma_, tk.lemma, tk.lower_, tk.lower, tk.dep_, [x.lower_ for x in tk.children])
+                   
+        elif(tk.pos_ == "NOUN"):
+           for x in tk.children:
+              if(x.pos_ == "PRON"):
+                if (x.lower_ == "her"):
+                   tot_her_pos += 1
+                   freq_her_pos[tk.lemma_] +=1
+                elif (x.lower_ == "his"):
+                   tot_his += 1
+                   freq_his[tk.lemma_] +=1
+                    
     except UnicodeDecodeError:
       pass
 
@@ -71,18 +80,26 @@ def calcPMI(pron, verb, freq, tot):
      pmi = 0
   return pmi
 
-verb = ""      
-while(verb != "Quit"):
-  verb = input("Enter a verb (Quit to quit, remember the quotation marks!): ")
-  pmi_she = calcPMI(u'she', verb, freq_s, tot_s)
-  pmi_he = calcPMI(u'he', verb, freq_h, tot_h)
-  pmi_her = calcPMI(u'her', verb, freq_her, tot_her)
-  pmi_him = calcPMI(u'him', verb, freq_him, tot_him)
-#  verbGivenShe = freq_s[verb]/tot_s
-#  verbGivenHe = freq_h[verb]/tot_h
-#  sheGivenVerb = (2**(nlp.vocab[u'she'].prob) * verbGivenShe) /2**(nlp.vocab[unicode(verb)].prob)
-#  heGivenVerb = (2**(nlp.vocab[u'he'].prob) * verbGivenHe) /2**(nlp.vocab[unicode(verb)].prob)
-#  pmi_she = sheGivenVerb/2**(nlp.vocab[u'she'].prob)
-#  pmi_he = heGivenVerb/2**(nlp.vocab[u'he'].prob)
-  # print ("Frequency for female: ", freq_s[verb]/tot_s, "Frequency for male: ", freq_h[verb]/tot_h)
-  print ("Frequency for she: ", pmi_she, "Frequency for he: ", pmi_he, "Frequency for her: ", pmi_her, "Frequency for him: ", pmi_him)
+def queryForVerbFreq():
+   verb = ""      
+   while True:
+     verb = input("Enter a verb (Quit to quit, remember the quotation marks!): ")
+     if(verb == "Quit"): return
+     pmi_she = calcPMI(u'she', verb, freq_s, tot_s)
+     pmi_he = calcPMI(u'he', verb, freq_h, tot_h)
+     pmi_her = calcPMI(u'her', verb, freq_her, tot_her)
+     pmi_him = calcPMI(u'him', verb, freq_him, tot_him)
+     # print ("Frequency for female: ", freq_s[verb]/tot_s, "Frequency for male: ", freq_h[verb]/tot_h)
+     print ("Frequency for she: ", pmi_she, "Frequency for he: ", pmi_he, "Frequency for her: ", pmi_her, "Frequency for him: ", pmi_him)
+     
+def queryForNounFreq():
+   noun = ""
+   while True:
+      noun = input("Enter a noun (Quit to quit, remember the quotation marks!): ")
+      if(noun == "Quit"): return
+      pmi_her_pos = calcPMI(u'her', noun, freq_her_pos, tot_her_pos)
+      pmi_his = calcPMI(u'his', noun, freq_his, tot_his)
+      print ("Frequency for her: ", pmi_her_pos, "Frequency for his: ", pmi_his)
+      
+queryForVerbFreq()
+queryForNounFreq()
