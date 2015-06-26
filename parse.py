fa90ce07f43fcd6b5b22ffba627d9f4928a6973b
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-06-26 21:56:47 +0000

    added freq of she or he and direct obj

--- parse.py
+++ parse.py
@@ -9,6 +9,8 @@ freq_her = defaultdict(int)
 freq_him = defaultdict(int)
 freq_her_pos = defaultdict(int)
 freq_his = defaultdict(int)
+freq_sobj = defaultdict(int)
+freq_hobj = defaultdict(int)
 tot_s = 0.0
 tot_h = 0.0
 tot_her = 0.0
@@ -24,10 +26,10 @@ nlp = spacy.en.English()
 #x = [1,2,3]
 #y = [z*z for z in x if z > 1]
 
-#def findObjFreq(tk, freq):
-#  for x in tk.children:
-#     if(x.pos_ == "NOUN"):
-#        freq[x.lemma_ += 1]
+def findObjFreq(tk, freq):
+  for x in tk.children:
+     if(x.pos_ == "NOUN"):
+        freq[x.lemma_] += 1
 
 for line in fileinput.input():
   id_, chapter = [x.rstrip() for x in line.split("\t")]
@@ -41,14 +43,13 @@ for line in fileinput.input():
            for x in tk.children:
               if(x.pos_ == "PRON"):
                 if (x.lower_ == "she"):
-                   # print (tk.lower_, x.lower_)
                    tot_s += 1
                    freq_s[tk.lemma_] +=1
+                   findObjFreq(tk, freq_sobj)
                 elif (x.lower_ == "he"):
-                   # print (tk.lower_, x.lower_)
                    tot_h += 1
                    freq_h[tk.lemma_] +=1
-                   # print freq_h[tk.lower_]
+                   findObjFreq(tk, freq_hobj)
                 elif (x.lower_ == "her"):
                    tot_her += 1
                    freq_her[tk.lemma_] +=1
@@ -70,17 +71,17 @@ for line in fileinput.input():
       pass
 
 
-def calcPMI(pron, verb, freq, tot):
-  if(tot != 0 and freq[verb] != 0):
-     verbGivenPron = freq[verb]/tot
-     pronGivenVerb = (2**(nlp.vocab[pron].prob) * verbGivenPron) /2**(nlp.vocab[unicode(verb)].prob)
-     x = pronGivenVerb/2**(nlp.vocab[pron].prob)
+def calcPMI(pron, word, freq, tot):
+  if(tot != 0 and freq[word] != 0):
+     wordGivenPron = freq[word]/tot
+     pronGivenWord = (2**(nlp.vocab[pron].prob) * wordGivenPron) /2**(nlp.vocab[unicode(word)].prob)
+     x = pronGivenWord/2**(nlp.vocab[pron].prob)
      pmi = math.log(x)
   else:
      pmi = 0
   return pmi
 
-def queryForVerbFreq():
+def queryForVerbFreq(freq_s, tot_s, freq_h, tot_h, freq_her, tot_her, freq_him, tot_him):
    verb = ""      
    while True:
      verb = input("Enter a verb (Quit to quit, remember the quotation marks!): ")
@@ -90,16 +91,20 @@ def queryForVerbFreq():
      pmi_her = calcPMI(u'her', verb, freq_her, tot_her)
      pmi_him = calcPMI(u'him', verb, freq_him, tot_him)
      # print ("Frequency for female: ", freq_s[verb]/tot_s, "Frequency for male: ", freq_h[verb]/tot_h)
-     print ("Frequency for she: ", pmi_she, "Frequency for he: ", pmi_he, "Frequency for her: ", pmi_her, "Frequency for him: ", pmi_him)
+     print ("Frequency for she: ", pmi_she, "Frequency for he: ", pmi_he)
+     print ("Frequency for her: ", pmi_her, "Frequency for him: ", pmi_him)
      
-def queryForNounFreq():
+def queryForNounFreq(freq_her_pos, tot_her_pos, freq_his, tot_his, freq_sobj, tot_s, freq_hobj, tot_h):
    noun = ""
    while True:
       noun = input("Enter a noun (Quit to quit, remember the quotation marks!): ")
       if(noun == "Quit"): return
       pmi_her_pos = calcPMI(u'her', noun, freq_her_pos, tot_her_pos)
       pmi_his = calcPMI(u'his', noun, freq_his, tot_his)
+      pmi_sobj = calcPMI(u'she', noun, freq_sobj, tot_s)
+      pmi_hobj = calcPMI(u'he', noun, freq_hobj, tot_h)
+      print ("Frequency for she: ", pmi_sobj, "Frequency for he: ", pmi_hobj)
       print ("Frequency for her: ", pmi_her_pos, "Frequency for his: ", pmi_his)
       
-queryForVerbFreq()
-queryForNounFreq()
+queryForVerbFreq(freq_s, tot_s, freq_h, tot_h, freq_her, tot_her, freq_him, tot_him)
+queryForNounFreq(freq_her_pos, tot_her_pos, freq_his, tot_his, freq_sobj, tot_s, freq_hobj, tot_h)
