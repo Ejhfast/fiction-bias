c2a30118026eaad37eb08a9d6e78fa85bed0db95
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-06-26 19:28:01 +0000

    Used correct pmi function (log) and added freq for him and her - need to test

--- parse.py
+++ parse.py
@@ -1,11 +1,16 @@
 import fileinput
 from collections import defaultdict
 import spacy.en
+import math
 
 freq_h = defaultdict(int)
 freq_s = defaultdict(int)
+freq_her = defaultdict(int)
+freq_him = defaultdict(int)
 tot_s = 0.0
 tot_h = 0.0
+tot_her = 0.0
+tot_him = 0.0
 
 # for k,v in freq_h.iteritems():
   # iterate over dictionary keys, values
@@ -24,7 +29,7 @@ nlp = spacy.en.English()
 for line in fileinput.input():
   id_, chapter = [x.rstrip() for x in line.split("\t")]
   id_ = int(id_)
-  for sen in chapter.split("."):
+  for sen in chapter.split("." or "!" or "?"):
     try:
       tokens = nlp(unicode(sen),tag=True,parse=True)
       for tk in tokens:
@@ -41,17 +46,29 @@ for line in fileinput.input():
                    tot_h += 1
                    freq_h[tk.lemma_] +=1
                    # print freq_h[tk.lower_]
+                elif (x.lower_ == "her"):
+                   tot_her += 1
+                   freq_her[tk.lemma_] +=1
+                elif (x.lower_ == "him"):
+                   tot_him += 1
+                   freq_him[tk.lemma_] +=1
              
-            
-          #print(tk.pos_, tk.lemma_, tk.lemma, tk.lower_, tk.lower, tk.dep_, [x.lower_ for x in tk.children])
+#         if(tk.pos_ == "PRON"):
+#            parent = tk.parent
+#            print(parent)
+            # print(tk.pos_, tk.lemma_, tk.lemma, tk.lower_, tk.lower, tk.dep_, [x.lower_ for x in tk.children])
     except UnicodeDecodeError:
       pass
 
 
 def calcPMI(pron, verb, freq, tot):
-  verbGivenPron = freq[verb]/tot
-  pronGivenVerb = (2**(nlp.vocab[pron].prob) * verbGivenPron) /2**(nlp.vocab[unicode(verb)].prob)
-  pmi = pronGivenVerb/2**(nlp.vocab[pron].prob)
+  if(tot != 0 and freq[verb] != 0):
+     verbGivenPron = freq[verb]/tot
+     pronGivenVerb = (2**(nlp.vocab[pron].prob) * verbGivenPron) /2**(nlp.vocab[unicode(verb)].prob)
+     x = pronGivenVerb/2**(nlp.vocab[pron].prob)
+     pmi = math.log(x)
+  else:
+     pmi = 0
   return pmi
 
 verb = ""      
@@ -59,6 +76,8 @@ while(verb != "Quit"):
   verb = input("Enter a verb (Quit to quit, remember the quotation marks!): ")
   pmi_she = calcPMI(u'she', verb, freq_s, tot_s)
   pmi_he = calcPMI(u'he', verb, freq_h, tot_h)
+  pmi_her = calcPMI(u'her', verb, freq_her, tot_her)
+  pmi_him = calcPMI(u'him', verb, freq_him, tot_him)
 #  verbGivenShe = freq_s[verb]/tot_s
 #  verbGivenHe = freq_h[verb]/tot_h
 #  sheGivenVerb = (2**(nlp.vocab[u'she'].prob) * verbGivenShe) /2**(nlp.vocab[unicode(verb)].prob)
@@ -66,4 +85,4 @@ while(verb != "Quit"):
 #  pmi_she = sheGivenVerb/2**(nlp.vocab[u'she'].prob)
 #  pmi_he = heGivenVerb/2**(nlp.vocab[u'he'].prob)
   # print ("Frequency for female: ", freq_s[verb]/tot_s, "Frequency for male: ", freq_h[verb]/tot_h)
-  print ("Frequency for female: ", pmi_she, "Frequency for male: ", pmi_he)
+  print ("Frequency for she: ", pmi_she, "Frequency for he: ", pmi_he, "Frequency for her: ", pmi_her, "Frequency for him: ", pmi_him)
