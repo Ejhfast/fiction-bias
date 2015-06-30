b1be4d784aa7f8ef3245e334fb93f37f43c95b76
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-06-30 04:46:44 +0000

    calc all PMIs and store

--- parse.py
+++ parse.py
@@ -11,6 +11,14 @@ freq_her_pos = defaultdict(int)
 freq_his = defaultdict(int)
 freq_sobj = defaultdict(int)
 freq_hobj = defaultdict(int)
+pmi_h = defaultdict(float)
+pmi_s = defaultdict(float)
+pmi_her = defaultdict(float)
+pmi_him = defaultdict(float)
+pmi_her_pos = defaultdict(float)
+pmi_his = defaultdict(float)
+pmi_sobj = defaultdict(float)
+pmi_hobj = defaultdict(float)
 tot_s = 0.0
 tot_h = 0.0
 tot_her = 0.0
@@ -81,30 +89,42 @@ def calcPMI(pron, word, freq, tot):
      pmi = 0
   return pmi
 
+def calcAllPMIs(pron, freq, tot, pmi):
+  for k,v in freq.iteritems():
+     pmi[k] = calcPMI(pron, k, freq, tot)
+     
+calcAllPMIs(u'she', freq_s, tot_s, pmi_s)
+calcAllPMIs(u'he', freq_h, tot_h, pmi_h)
+calcAllPMIs(u'her', freq_her, tot_her, pmi_her)
+calcAllPMIs(u'him', freq_him, tot_him, pmi_him)
+calcAllPMIs(u'her', freq_her_pos, tot_her_pos, pmi_her_pos)
+calcAllPMIs(u'his', freq_his, tot_his, pmi_his)
+calcAllPMIs(u'she', freq_sobj, tot_s, pmi_sobj)
+calcAllPMIs(u'he', freq_hobj, tot_h, pmi_hobj)
+
 def queryForVerbFreq(freq_s, tot_s, freq_h, tot_h, freq_her, tot_her, freq_him, tot_him):
    verb = ""      
    while True:
      verb = input("Enter a verb (Quit to quit, remember the quotation marks!): ")
      if(verb == "Quit"): return
-     pmi_she = calcPMI(u'she', verb, freq_s, tot_s)
-     pmi_he = calcPMI(u'he', verb, freq_h, tot_h)
-     pmi_her = calcPMI(u'her', verb, freq_her, tot_her)
-     pmi_him = calcPMI(u'him', verb, freq_him, tot_him)
-     # print ("Frequency for female: ", freq_s[verb]/tot_s, "Frequency for male: ", freq_h[verb]/tot_h)
-     print ("Frequency for she: ", pmi_she, "Frequency for he: ", pmi_he)
-     print ("Frequency for her: ", pmi_her, "Frequency for him: ", pmi_him)
+#     pmi_she = calcPMI(u'she', verb, freq_s, tot_s)
+#     pmi_he = calcPMI(u'he', verb, freq_h, tot_h)
+#     pmi_her = calcPMI(u'her', verb, freq_her, tot_her)
+#     pmi_him = calcPMI(u'him', verb, freq_him, tot_him)
+     print ("Frequency for she: ", pmi_s[verb], "Frequency for he: ", pmi_h[verb])
+     print ("Frequency for her: ", pmi_her[verb], "Frequency for him: ", pmi_him[verb])
      
 def queryForNounFreq(freq_her_pos, tot_her_pos, freq_his, tot_his, freq_sobj, tot_s, freq_hobj, tot_h):
    noun = ""
    while True:
       noun = input("Enter a noun (Quit to quit, remember the quotation marks!): ")
       if(noun == "Quit"): return
-      pmi_her_pos = calcPMI(u'her', noun, freq_her_pos, tot_her_pos)
-      pmi_his = calcPMI(u'his', noun, freq_his, tot_his)
-      pmi_sobj = calcPMI(u'she', noun, freq_sobj, tot_s)
-      pmi_hobj = calcPMI(u'he', noun, freq_hobj, tot_h)
-      print ("Frequency for she: ", pmi_sobj, "Frequency for he: ", pmi_hobj)
-      print ("Frequency for her: ", pmi_her_pos, "Frequency for his: ", pmi_his)
+#      pmi_her_pos = calcPMI(u'her', noun, freq_her_pos, tot_her_pos)
+#      pmi_his = calcPMI(u'his', noun, freq_his, tot_his)
+#      pmi_sobj = calcPMI(u'she', noun, freq_sobj, tot_s)
+#      pmi_hobj = calcPMI(u'he', noun, freq_hobj, tot_h)
+      print ("Frequency for she: ", pmi_sobj[noun], "Frequency for he: ", pmi_hobj[noun])
+      print ("Frequency for her: ", pmi_her_pos[noun], "Frequency for his: ", pmi_his[noun])
       
 queryForVerbFreq(freq_s, tot_s, freq_h, tot_h, freq_her, tot_her, freq_him, tot_him)
 queryForNounFreq(freq_her_pos, tot_her_pos, freq_his, tot_his, freq_sobj, tot_s, freq_hobj, tot_h)
