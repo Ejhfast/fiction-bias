a7d58f701c028e51eddd77ba4eec5578bebee7ae
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-07-07 00:14:35 +0000

    added std dev and query option

--- biasgenres.py
+++ biasgenres.py
@@ -2,6 +2,7 @@ import math
 from collections import defaultdict
 import cPickle as pickle
 import spacy.en
+import numpy
 
 f = open("cat00000.pmis.txt", "r")
 
@@ -17,12 +18,13 @@ class Cat:
      self.pmi_hobj = defaultdict(float)
 
 cats = {}
+diffs = {}
 for x in range (0, 25):
   cats[x] = Cat()
+  diffs[x] = []
   
 for x in range (0, 25):
    cat = pickle.load(f)
-   print(cat)
    cats[cat].pmi_h = pickle.load(f)
    cats[cat].pmi_s = pickle.load(f)
    cats[cat].pmi_her = pickle.load(f)
@@ -39,6 +41,7 @@ def avepmi(cat, pmih, pmis, pronh, prons):
       if((pmis[k] != 0) and (pmih[k] != 0)):
          count += 1
          numer += abs((pmis[k] - pmih[k]))
+         diffs[cat].append(abs((pmis[k] - pmih[k])))
    if(count != 0):
       ave = numer/count
       #print("Average for", cat, "between pronouns", pronh, prons, "is:", ave)
@@ -51,24 +54,36 @@ for x in range (0, 25):
    avehsobj = avepmi(x, cats[x].pmi_hobj, cats[x].pmi_sobj, "hobj", "sobj")
    avehhpos = avepmi(x, cats[x].pmi_his, cats[x].pmi_her_pos, "his", "her_pos")
    total = (avehs + avehh + avehsobj + avehhpos)/4.0
-   print("Average for", x, "is: ", total)
+   if total != 0:
+      std = numpy.std(diffs[x])
+   else:
+      std = 0
+   print("Average for", x, "is: ", total, "Std dev is: ", std)
 
-#def queryForVerbFreq(pmi_s, pmi_h, pmi_her, pmi_him):
-#   verb = ""      
-#   while True:
-#     verb = input("Enter a verb (Quit to quit, remember the quotation marks!): ")
-#     if(verb == "Quit"): return
-#     print ("Frequency for she: ", pmi_s[verb], "Frequency for he: ", pmi_h[verb])
-#     print ("Frequency for her: ", pmi_her[verb], "Frequency for him: ", pmi_him[verb])
-#     
-#def queryForNounFreq(pmi_sobj, pmi_hobj, pmi_her_pos, pmi_his):
-#   noun = ""
-#   while True:
-#      noun = input("Enter a noun (Quit to quit, remember the quotation marks!): ")
-#      if(noun == "Quit"): return
-#      print ("Frequency for she: ", pmi_sobj[noun], "Frequency for he: ", pmi_hobj[noun])
-#      print ("Frequency for her: ", pmi_her_pos[noun], "Frequency for his: ", pmi_his[noun])
-#
-#     
-#queryForVerbFreq(pmi_s, pmi_h, pmi_her, pmi_him)
-#queryForNounFreq(pmi_sobj, pmi_hobj, pmi_her_pos, pmi_his)
+def queryForVerbFreq():
+   verb = ""      
+   while True:
+     verb = input("Enter a verb (Quit to quit, remember the quotation marks!): ")
+     if(verb == "Quit"): return
+     while True:
+        ct = input("Enter a genre(-1 to quit): ")
+        if(ct == -1): return
+        print ("Frequency for she: ", cats[ct].pmi_s[verb], "Frequency for he: ", cats[ct].pmi_h[verb])
+        print ("Frequency for her: ", cats[ct].pmi_her[verb], "Frequency for him: ", cats[ct].pmi_him[verb])
+     #queryForGenre(verb, "pmi_s", "pmi_h", "pmi_her", "pmi_him")
+     
+     
+def queryForNounFreq():
+   noun = ""
+   while True:
+      noun = input("Enter a noun (Quit to quit, remember the quotation marks!): ")
+      if(noun == "Quit"): return
+      while True:
+        ct = input("Enter a genre(-1 to quit): ")
+        if(ct == -1): return
+        print ("Frequency for she: ", cats[ct].pmi_sobj[noun], "Frequency for he: ", cats[ct].pmi_hobj[noun])
+        print ("Frequency for her: ", cats[ct].pmi_her_pos[noun], "Frequency for him: ", cats[ct].pmi_his[noun])
+      #queryForGenre(noun, "pmi_sobj", "pmi_hobj", "pmi_her_pos", "pmi_his")
+     
+queryForVerbFreq()
+queryForNounFreq()
