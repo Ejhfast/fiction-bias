48bf8fd7464a78b59d1a96f0defef747b1154bcd
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-07-22 00:46:47 +0000

    countChars works, ran on small data set, added to bechtel but untested

--- parsestories.py
+++ parsestories.py
@@ -4,21 +4,23 @@ import spacy.en
 import math
 import cPickle as pickle
 from sets import Set
+import names
 
 N = 100000
 CONVOLENGTH = 5
 BUFFERLENGTH = 3
 
 stories = {}
+categories = {}
+writers = {}
+ratings = {}
+
 f = open("/home/ubuntu/ebs/dataset/story_chapters/part-m-00000", "r")
 for line in f:
   storyID, textID = [x.rstrip() for x in line.split("\t")]
   stories[textID] = storyID
 f.close
 
-categories = {}
-writers = {}
-ratings = {}
 f = open("/home/ubuntu/ebs/dataset/story_details/part-m-00000", "r")
 for line in f:
   writerID, storyID, categoryID, rating = [x.rstrip() for x in line.split("\t")]
@@ -27,43 +29,27 @@ for line in f:
   ratings[storyID] = rating
 f.close 
 
-storyBechFreq = defaultdict(int)
-cats = defaultdict(int)
+class Cat:
+  def __init__(self):
+     self.storyBechFreq = defaultdict(int)
+     self.failed = []
+     self.passed = []
+
+cats = {}
+for x in range (0, 25):
+  cats[x] = Cat()
 
 nlp = spacy.en.English()
  
 def speakerverb(word):
    if(word == 'say' or word == 'answer' or word == 'reply' or word == 'shout' or word == 'whisper' or word == 'yell'
     or word == 'whine' or word == 'stammer' or word == 'respond' or word == 'remind' or word == 'remark' or word == 'utter'
-    or word == 'recall' or word == 'ask' or word == 'hint' or word == 'groan' or word == 'claim'):
+    or word == 'recall' or word == 'ask' or word == 'hint' or word == 'groan' or word == 'claim' or word == 'declare' or word == 'describe'
+    or word == 'interject' or word == 'mutter' or word == 'mumble' or word == 'continue' or word == 'clarify'):
        return True
    else:
        return False 
 
-def readNames(filename):
-  f = open(filename, 'r')
-  names = Set([])
-  for line in f:
-    name, freq, index, url = [x.rstrip() for x in line.split(",")]
-    name = name.replace("\"","")
-    names.add(name.lower())
-  return names
-
-malnames = readNames("/home/ubuntu/ebs/male-names-freq.csv")
-femnames = readNames("/home/ubuntu/ebs/female-names-freq.csv")
-
-def ismalename(name):  
-  if (name in malnames):
-     return True
-  else:
-     return False     
-
-def isfemalename(name):
-  print (name)
-  if (name in femnames):
-     return True
-  else:
-     return False  
        
 def bechdel(chapter, cat):
   first = True
@@ -83,8 +69,10 @@ def bechdel(chapter, cat):
        cats[cat] += 1
     try:
       tokens = nlp(unicode(sen),tag=True,parse=True)
+      dialog = False
       for tk in tokens:
-        if (tk.lower == 'he' or ismalename(tk.lower_)):
+        if(tk.lower_ == "\""): dialog = True
+        if ((tk.lower_ == 'he' or names.ismalename(tk.lower_)) and dialog == True):
            break
            first = True
            start = False
@@ -92,18 +80,15 @@ def bechdel(chapter, cat):
            countBuffer = 0
            name = ""
         if(start == False and tk.pos_ == "VERB" and speakerverb(tk.lemma_)):
-           print(tk.lemma_)
            for x in tk.children:
-              if(isfemalename(x.lower_) and first == True):
-                 print("first to false")
+              if(names.isfemalename(x.lower_) and first == True):
                  name = x.lower_
                  first = False
-              elif (x.lower_ != name and isfemalename(x.lower_)):
+              elif (x.lower_ != name and names.isfemalename(x.lower_)):
                  start = True
-                 print("start to true")
       if (first == False):
          countBuffer += 1
-      if (start == True):
+      if (start == True and dialog == True):
          countConvo += 1
          
     except UnicodeDecodeError:
@@ -120,6 +105,14 @@ for line in fileinput.input():
   rating = int(ratings[storyid])
   cat = int(categories[storyid])
   if(bechdel(chapter, cat)):
-     storyBechFreq[storyid] += 1
+     cats[cat].storyBechFreq[storyid] += 1
+     cats[cat].passed.append(storyid)
+  else:
+     cats[cat].failed.append(storyid)
 
-print storyBechFreq
+f = open("bechdeltesetscateg.txt", 'w')
+
+for cat in range (0, 25):
+   for key in sorted(cats[cat].storyBechFreq):
+      f.write(key + "," + str(cats[cat].storyBechFreq[key]) + "\n")
+   f.write("Num pass bechtel:" + str(len(cats[cat].passed)) + " Num fail bechtel:" + str(len(cats[cat].failed)))
