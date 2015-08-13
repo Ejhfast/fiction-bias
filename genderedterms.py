647fdbf71cdbe66f8b0684332a2c0fa3ce279871
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-08-13 17:32:26 +0000

    add local genderedterms

---
+++ genderedterms.py
@@ -0,0 +1,25 @@
+from sets import Set
+
+f = open ("genderedterms.txt", 'r')
+
+malnames = Set([])
+femnames = Set([])
+
+for line in f:
+  name, gender = [x.rstrip() for x in line.split(" ")]
+  if(gender == 'f'):
+    femnames.add(name)
+  else:
+    malnames.add(name)
+
+def ismaleterm(name):  
+  if (name in malnames):
+     return True
+  else:
+     return False     
+
+def isfemaleterm(name):
+  if (name in femnames):
+     return True
+  else:
+     return False  
