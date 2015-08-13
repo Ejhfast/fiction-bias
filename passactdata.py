f32fc3c0d03c12954b36aa555f4cb5b5f3508e76
Ubuntu <ubuntu@ip-172-31-17-70.us-west-2.compute.internal>
2015-08-13 17:28:50 +0000

    local file passact

--- passactdata.py
+++ passactdata.py
@@ -1,6 +1,6 @@
 verbs = {}
 
-f = open("/home/ubuntu/ebs/passactverbs.txt", 'r')
+f = open("passactverbs.txt", 'r')
 for line in f:
   verb, typ, freq = [x.rstrip() for x in line.split("\t")]
   verbs[verb] = typ
